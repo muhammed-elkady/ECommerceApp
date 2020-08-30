7bfdb58a39ea67e5027b505e12931961dcd30be9
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2020-08-30 17:38:23 +0200

    added entity framework core

---
+++ ECommerceApp.Infrastructure/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Infrastructure
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        { }
+        
+        public DbSet<Product> Products { get; set; }
+
+    }
+}

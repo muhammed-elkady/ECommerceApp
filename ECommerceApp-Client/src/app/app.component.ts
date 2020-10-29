d9eed9dbbbc3032fea929fb77c1e3077d59e6688
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2020-10-29 16:51:56 +0200

    added nav bar + initial api call to products from client

--- ECommerceApp-Client/src/app/app.component.ts
+++ ECommerceApp-Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ECommerce App';
+  products: Product[];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:44375/api/products?pageSize=50')
+      .subscribe((res: Pagination<Product>) => {
+        this.products = res.data;
+      }, err => {
+        console.log(err);
+      })
+  }
 }

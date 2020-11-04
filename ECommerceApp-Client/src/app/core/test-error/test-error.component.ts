4cd3a8286fe2a7602d1d5e5bea2e7f4879b49a6a
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2020-11-04 17:24:56 +0200

    not-found and server-error handle in client

---
+++ ECommerceApp-Client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42')
+      .subscribe(res => {
+
+      },
+        err => {
+
+        })
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror')
+      .subscribe(res => {
+
+      },
+        err => {
+
+        })
+  }
+
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest')
+      .subscribe(res => {
+
+      },
+        err => {
+
+        })
+  }
+
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'products/sdaofhoalj')
+      .subscribe(res => {
+
+      },
+        err => {
+
+        })
+  }
+
+}

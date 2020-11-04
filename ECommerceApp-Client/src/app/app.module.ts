7adb26221afbb03282b0617cc27cdd3bfc32e816
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2020-11-04 16:03:53 +0200

    routing correct

--- ECommerceApp-Client/src/app/app.module.ts
+++ ECommerceApp-Client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    ShopModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
